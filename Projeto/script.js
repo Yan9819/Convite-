47da2f7227d93e36284c7cb42616041e8b23ebfd
Yan9819 <yanvasco098@gmail.com>
2026-04-02 15:51:26 -0300

    Update script.js

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,1 +1,24 @@
+window.onload = function () {
+  const botaoNao = document.getElementById("nao");
 
+  function fugir() {
+    const larguraJanela = window.innerWidth;
+    const alturaJanela = window.innerHeight;
+
+    const larguraBotao = botaoNao.offsetWidth;
+    const alturaBotao = botaoNao.offsetHeight;
+
+    const novaPosicaoX = Math.random() * (larguraJanela - larguraBotao);
+    const novaPosicaoY = Math.random() * (alturaJanela - alturaBotao);
+
+    botaoNao.style.left = novaPosicaoX + "px";
+    botaoNao.style.top = novaPosicaoY + "px";
+  }
+
+  botaoNao.addEventListener("mouseover", fugir);
+  botaoNao.addEventListener("touchstart", fugir);
+};
+
+function aceitou() {
+  window.location.href = "aceitou.html";
+}
